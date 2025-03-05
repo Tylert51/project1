f1ac33a2995ef882112e1ccce8a437009399e6af
Tyler Tang <tylertang50@gmail.com>
2025-03-04 23:37:45 -0500

    handled user input and error checking

--- shapes/src/DrawingShapes.java
+++ shapes/src/DrawingShapes.java
@@ -1,7 +1,69 @@
+import java.util.Scanner;
+
 public class DrawingShapes {
 
     public static void main(String[] args) {
-        
+        Scanner sc = new Scanner(System.in);
+
+        System.out.println("Enter a shape: r t h o p");
+        String check = "r t h o p";
+
+        String shape = sc.nextLine();
+        if(!check.contains(shape)) {    // If invalid shape
+            System.out.println("Invalid shape\nGoodbye!");
+            System.exit(0);
+        }
+
+        System.out.println("Enter a length");
+        int len = sc.nextInt();
+
+        if(len <= 1) {
+            System.out.println("Length must be greater than 1\nGoodbye!");
+            System.exit(0);
+        }
+
+        switch (shape) {
+            case "r" -> {
+
+                System.out.println("Enter a height");
+                int height = sc.nextInt();
+
+                if (height <= 1) {   //If invalid height
+                    System.out.println("Height must be greater than 1\nGoodbye!");
+                    System.exit(0);
+                }
+
+                System.out.println("Below is a " + len + " by " + height + " rectangle of *");
+                drawRectangle(len, height);
+
+            }
+            case "t" -> {
+
+                System.out.println("Below is a triangle with two side lengths of " + len + " *");
+                drawTriangle(len);
+
+            }
+            case "h" -> {
+
+                System.out.println("Below is a hexagon with side lengths of " + len + " *");
+                drawHexagon(len);
+
+            }
+            case "o" -> {
+
+                System.out.println("Below is a octagon with side lengths of " + len + " *");
+                drawOctagon(len);
+
+            }
+            case "p" -> {
+
+                System.out.println("Below is a pentagon with 4 side lengths of " + len + " *");
+                drawPentagon(len);
+
+            }
+        }
+
+
     }
 
     // Code to draw Rectangle
@@ -14,7 +76,7 @@ public class DrawingShapes {
         }
     }
 
-    // Code to draw Traingle
+    // Code to draw Triangle
     public static void drawTriangle(int l) {
         int base = 2 * (l - 1) + 1;     // "max" number of dots in each line
         int numWhiteSpace;
@@ -24,12 +86,12 @@ public class DrawingShapes {
             for(int j = 0; j < base; j++) {
                 numWhiteSpace = (base - numDots) / 2;
                 
-                if(j < numWhiteSpace) {     // makes sure the intial white spaces are printed out before the dots
+                if(j < numWhiteSpace) {     // makes sure the initial white spaces are printed out before the dots
                     printDot = false;
                 } else if (j < numWhiteSpace + numDots) {   // ending index of dots
                     printDot = true;
                 } else {
-                    break;      // once all dots are printed berak so that no extra stuff is printed after
+                    break;      // once all dots are printed break so that no extra stuff is printed after
                 }
 
                 System.out.print( ((printDot) ? "*" : " " ) );  // based off status of printing dot, will either print out a dot or whitespace
@@ -49,7 +111,7 @@ public class DrawingShapes {
 
         for(int i = 0; i < l * 2 - 1; i++) {
             for(int j = 0; j < base; j++) {
-                numWhiteSpace = (base - numDots) / 2;   // calculates white space needed based off the number of dots on the line
+                numWhiteSpace = (base - numDots) / 2;   // calculates white space needed to be based off the number of dots on the line
                 
                 // changes boolean to determine whether a dot or whitespace gets printed
                 if(j < numWhiteSpace) {     
@@ -96,11 +158,11 @@ public class DrawingShapes {
             }
             System.out.println();
 
-            if(i < l - 1) {     // before reaching widest part, dots increase by 2
+            if(i < l - 1) {     // before reaching the widest part, dots increase by 2
                 increment = 2;
-            } else if (i < 2 * l - 2) {     // at widest part, num dots remains costant (no increment) for length l
+            } else if (i < 2 * l - 2) {     // at widest part, num dots remains constant (no increment) for length l
                 increment = 0;
-            } else {        // after this, dots decrase by 2 each new line
+            } else {        // after this, dots decrease by 2 each new line
                 increment = -2;
             }
             numDots += increment;
@@ -109,10 +171,10 @@ public class DrawingShapes {
 
     // code to draw pentagon
     public static void drawPentagon(int l) {
-        int base = (2 * l) - 1;     // widest part of pentgon
+        int base = (2 * l) - 1;     // widest part of pentagon
 
-        // composed of a trangle of lenght l, and a rectangle with height of (l-1) and a width of base
-        drawTriangle(l);            
+        // composed of a triangle of length l, and a rectangle with height of (l-1) and a width of base
+        drawTriangle(l);
         drawRectangle(base, l - 1);
     }
 }
